f534ddf8940910c8ef8d45d9604fd1f348821f92
jchenriquez <jc.henriquez04@gmail.com>
2020-02-19 13:14:42 -0500

    This solution would get and I, J and will compute i+j = -c and looking if -c is in set.

--- threesum.go
+++ threesum.go
@@ -1,6 +1,6 @@
 package threesum
 
-func threeSum(sums []int) {
+func threeSum(sums []int) (result [][]int) {
 	negatedMp := make(map[int]map[int]bool)
 
 	for index, sum := range sums {
@@ -10,11 +10,30 @@ func threeSum(sums []int) {
 
 	for i := 0; i < len(sums); i++ {
 
+		valAtIndex := sums[i]
+		st, _ := negatedMp[valAtIndex*-1]
+
+		delete(st, i)
+
 		for j := 0; j < len(sums); j++ {
 			if j == i {
 				continue
 			}
 
+			sm := sums[i] + sums[j]
+
+			stTmp, _ := negatedMp[sm]
+
+			if len(stTmp) != 0 {
+				for ind := range stTmp {
+					result = append(result, []int{i, j, ind})
+				}
+			}
+
 		}
+
+		st[i] = true
 	}
+
+	return
 }
