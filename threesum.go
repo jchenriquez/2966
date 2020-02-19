3e35190daccab6620ff736730c8f1038b3e891b3
jchenriquez <jc.henriquez04@gmail.com>
2020-02-19 13:04:14 -0500

    pushing progress

--- threesum.go
+++ threesum.go
@@ -1,7 +1,20 @@
 package threesum
 
-func threeSum (sums []int) {
-  negatedMp := make(map[int]map[int]bool)
+func threeSum(sums []int) {
+	negatedMp := make(map[int]map[int]bool)
 
+	for index, sum := range sums {
+		st, _ := negatedMp[sum*-1]
+		st[index] = true
+	}
 
+	for i := 0; i < len(sums); i++ {
+
+		for j := 0; j < len(sums); j++ {
+			if j == i {
+				continue
+			}
+
+		}
+	}
 }
